b366e35943c51cde26fedc0a2dc7268803a8852e
Zhi Shen Yong <zhishenyong@live.com>
2017-06-18 11:38:54 +0800

    creating tree function

--- src/sgdevice/Device.java
+++ src/sgdevice/Device.java
@@ -1,29 +1,7 @@
 package sgdevice;
 
-public class Device {
+public class Device<T> {
 	
-	public String orientation;
-	public int degree;
-	
-	public Device() {
-		this.orientation = null;
-		this.degree = 0;
-	}
-	
-	public void setDegree(int degree) {
-		this.degree = degree;
-	}
-	
-	public void setOrientation(String orientation) {
-		this.orientation = orientation;
-	}
-	
-	public String getOrientation() {
-		return this.orientation;
-	}
-	
-	public int getDegree() {
-		return this.degree;
-	}
+	private T 
 
 }
--- src/sgdevice/Run.java
+++ src/sgdevice/Run.java
@@ -1,5 +1,6 @@
 package sgdevice;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Run {
@@ -7,7 +8,6 @@ public class Run {
 	public static void orientationChooser(Device device) {
 
 		Scanner reader = new Scanner(System.in);
-		
 
 		String z = new String("z");
 		String x = new String("x");
@@ -30,32 +30,34 @@ public class Run {
 		} else {
 			device.setOrientation(orientation);
 		}
-
 	}
 
 	public static void main(String[] args) {
-		
+
+		/*
 		System.out.println("How many SG Devices for this simulation?");
 		Scanner reader = new Scanner(System.in);
 		int SGCount = reader.nextInt();
 		Device[] list = new Device[SGCount];
-		
+
 		for (int i = 0; i < list.length; i++) {
-			System.out.println("Choose an orientation [z][x][other] for SG Device #" + (i+1));
+			System.out.println("Choose an orientation [z][x][other] for SG Device #" + (i + 1));
 			list[i] = new Device(); // instantiate each object
 			orientationChooser(list[i]);
 		}
-		
+
 		for (int i = 0; i < list.length; i++) {
 			String other = new String("other");
 			if (list[i].getOrientation().equals(other)) {
-				System.out.println("Orientation #" + (i+1) + ": " + list[i].getDegree());
+				System.out.println("Orientation #" + (i + 1) + ": " + list[i].getDegree());
 			} else {
-				System.out.println("Orientation #" + (i+1) + ": " + list[i].getOrientation());
+				System.out.println("Orientation #" + (i + 1) + ": " + list[i].getOrientation());
 			}
 		}
+		*/
 		
-
+		MyTreeNode<Device> root = new MyTreeNode<Device>("Hello", 90);
+		System.out.println(root.getData());
+		
 	}
-
 }
