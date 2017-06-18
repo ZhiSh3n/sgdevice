bcb51d4236b5140944faf9584f4feb22045c0a63
Zhi Shen Yong <zhishenyong@live.com>
2017-06-17 21:44:02 -0300

    int prompter class

--- src/sgdevice/Run.java
+++ src/sgdevice/Run.java
@@ -1,9 +1,20 @@
 package sgdevice;
 
+import java.util.Scanner;
+
 public class Run {
+	
+	public static int integerReader() {
+		Scanner temporaryReader = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int temporaryInteger = temporaryReader.nextInt();
+		return temporaryInteger;
+	}
 
 	public static void main(String[] args) {
-		
+		System.out.println("How many SG devices?");
+		int sgCount = integerReader();
+		System.out.println("You have " + sgCount + " SG Devices.");
 	}
 
 }
