ce6bcd914b99a670ef1ba3a488611bb9e86cbdf1
ZhiSh3n <zhishenyong@live.com>
2017-06-18 15:24:28 +0800

    changed mover

--- src/sgdevice/Run.java
+++ src/sgdevice/Run.java
@@ -8,56 +8,70 @@ public class Run {
 		Scanner scanner = new Scanner(System.in);
 		Mover root = new Mover(0, null);
 		Mover currentMover = root;
-		view(currentMover);
-		currentMover.addChild();
-		view(currentMover);
-		
-		
-		/* COMMANDS TO USE
-		currentMover = next(currentMover);
-		currentMover.addChild();
-		currentMover = previous(currentMover);
-		view(currentMover);
-		*/
-		
+
 		/*
+		 * COMMANDS TO USE currentMover = next(currentMover);
+		 * currentMover.addChild(); currentMover = previous(currentMover);
+		 * view(currentMover);
+		 */
+
 		while (true) {
+			System.out.println("_____________________________________________________________");
 			System.out.println("Commands: ");
-			System.out.println("   [goto child] : navigate to ");
-			System.out.println("   [goto parent] : ");
-			System.out.println("   [goto sibling] : ");
+			System.out.println("   [view] : view node information");
+			System.out.println("   [goto child] : navigate to child number # (starting from 0)");
+			System.out.println("   [goto parent] : navigate to the parent");
+			System.out.println("   [goto sibling] : navigate to a sibling #");
+			System.out.println("   [add child] : add a child (you will be promped to define an SG Device");
 			String line = scanner.nextLine();
+
+			switch (line) {
+			case "view":
+				view(currentMover);
+				break;
+			case "goto child":
+				currentMover = next(currentMover);
+				break;
+			case "goto parent":
+				currentMover = previous(currentMover);
+				break;
+			case "goto sibling":
+				break;
+			case "add child":
+				currentMover.addChild();
+				break;
+			default:
+			}
+			
 		}
-		*/
-		
+
 	}
 
 	// move to the next layer, specifying a child number (0, 1, 2 ..)
-	public static Mover next(Mover mover, int childNumber) {
-		if (mover.children.size() == 0) { // if no children
-			System.out.println("This mover has no children.");
+	public static Mover next(Mover mover) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the child number: ");
+		int childNumber = scanner.nextInt();
+		if (childNumber >= mover.children.size()) {
+			System.out.println("That child does not exist.");
 			return mover;
 		} else {
-			if (childNumber >= mover.children.size()) {
-				System.out.println("That child does not exist.");
-				return mover;
-			} else {
-				return mover.children.get(childNumber);
-			}
+			return mover.children.get(childNumber);
 		}
 	}
-	
-	// move to the previous node
+
+	// go to parent
 	public static Mover previous(Mover mover) {
 		if (mover.parent == null) {
 			System.out.println("You are already in the root node!");
 			return mover;
 		} else {
+			System.out.println("You have moved from layer " + mover.layer + " to layer " + mover.parent.layer);
 			return mover.parent;
 		}
 	}
-	
-	// view current node information 
+
+	// view current node information
 	public static void view(Mover mover) {
 		System.out.println("This node has " + mover.children.size() + " children.");
 		System.out.println("This node is in Layer: " + mover.layer);
@@ -68,7 +82,6 @@ public class Run {
 		} else {
 			System.out.println(mover.device.orientation);
 		}
-		System.out.println("");
 	}
-	
+
 }
